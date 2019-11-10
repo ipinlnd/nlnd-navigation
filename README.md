ed39576a02007bb4dceb00ee8908ce6945a4c519
ipinlnd <nlndipi@hotmail.com>
2019-11-10 16:12:01 +0330

    Initial commit

---
+++ README.md
@@ -0,0 +1,82 @@
+## NLND Navigation
+
+This is a simple view-manager for react-native that get's a group of routes and stacks and creates a view-manager which can act as the navigation system of the app.
+
+This module does not provide any sort of UI navigation components like Tab navigations or Drawer navigations. It only provides the basic system where you can use the navigation functions to do what you want.
+
+### Sample
+
+This is a simple example to see how it works:
+
+```js
+import { NlndNavigation } from "nlnd-navigation";
+
+const stacks = {
+  name: "root",
+  initialStack: "homestack",
+  stacks: [
+    {
+      name: "homestack",
+      initialRoute: "home",
+      routes: [{ key: "home", route: Home }, { key: "page1", route: Page1 }],
+      stacks: [
+        {
+          name: "page2stack",
+          initialRoute: "page2",
+          routes: [{ key: "page2", route: Page2 }]
+        },
+        {
+          name: "page3stack",
+          initialRoute: "page3",
+          routes: [{ key: "page3", route: Page3 }]
+        }
+      ]
+    }
+  ]
+};
+
+const App = () => {
+  return <NlndNavigation stack={stacks} />;
+};
+```
+
+And you can do the navigations like so:
+
+This is an example of in-stack navigations:
+
+```js
+<Button onPress={() => props.navigation.navigate("page1")} title="Page 1" />
+```
+
+To do inter-stack navigations, we provide a function that would route to your desired destination from root:
+
+```js
+<Button
+  onPress={() =>
+    props.navigation.navigateFromRoot(
+      ["root", "homestack", "page2stack"],
+      "page2"
+    )
+  }
+  title="Page 2"
+/>
+```
+
+This will route from root, to homestack, to page2stack and finally to the page2 component.
+
+### Refrences
+
+| Function                               | Return type                                                                                  | Description                                                       |
+| -------------------------------------- | -------------------------------------------------------------------------------------------- | ----------------------------------------------------------------- |
+| getStacks()                            | string                                                                                       | Will return a comma seperated list of all stacks                  |
+| getRoutes()                            | string                                                                                       | Will return a comma seperated list of all routes                  |
+| getCurrentStack()                      | string                                                                                       | Will return the name of the current stack                         |
+| getCurrentRoute()                      | string                                                                                       | Will return the name of the current route                         |
+| goBack()                               | boolean                                                                                      | Goes back to the previous route. returns false on failure.        |
+| goHome()                               | boolean                                                                                      | Goes to the initial route of the stack. returns false on failure. |
+| navigate(key, props)                   | boolean                                                                                      | Will navigate to the given route, inside the stack.               |
+| navigateFromRoot(stacks[], key, props) | Will route to the given key, starting from the root stack and moving along the stacks array. |
+
+### Authors
+
+Ali Rezaee <nlndipi@hotmail.com>
---
+++ index.tsx
@@ -0,0 +1,241 @@
+import React, { Component } from "react";
+import { View, StyleProp, ViewStyle, BackHandler } from "react-native";
+
+interface RouteProp {
+  key: string;
+  route: (props?: any) => JSX.Element;
+}
+
+interface StackProp {
+  name: string;
+  initialRoute?: string;
+  initialStack?: string;
+  routes?: RouteProp[];
+  stacks?: StackProp[];
+}
+
+interface Props {
+  stack: StackProp;
+  style?: StyleProp<ViewStyle>;
+}
+
+interface State {
+  currentStack: Stack;
+  currentRoute: Route;
+  currentProps: any;
+}
+
+interface Navigation {
+  getStacks(): string;
+  getRoutes(): string;
+  getCurrentStack(): string;
+  getCurrentRoute(): string;
+  goBack(): boolean;
+  goHome(): boolean;
+  navigate(key: string, props?: any): boolean;
+  navigateFromRoot(stacks: string[], key: string, props?: any): boolean;
+  rootStack: Stack;
+  initialStack: Stack;
+  initialRoute: Route;
+}
+
+interface Stack {
+  name: string;
+  routes?: Route[];
+  stacks?: Stack[];
+  initial?: Route;
+  parentStack?: Stack;
+}
+
+interface Route {
+  key: string;
+  route: () => JSX.Element;
+  parentStack: Stack;
+  previousRoute?: Route;
+}
+
+class NlndNavigation extends Component<Props, State> {
+  navigation: Navigation;
+
+  getRoute = (props: RouteProp, stack: Stack) => {
+    const route: Route = {
+      key: props.key,
+      route: props.route,
+      parentStack: stack
+    };
+
+    return route;
+  };
+
+  getStack = (props: StackProp, parentStack?: Stack) => {
+    const stack: Stack = {
+      name: props.name
+    };
+
+    if (props.routes) {
+      stack.routes = props.routes!.map(item => this.getRoute(item, stack));
+      stack.initial = stack.routes.filter(
+        item => item.key === props.initialRoute
+      )[0];
+    }
+    if (props.stacks) {
+      stack.stacks = props.stacks!.map(item => this.getStack(item, stack));
+    }
+    stack.parentStack = parentStack;
+
+    return stack;
+  };
+
+  getStacks = (stack: Stack): string => {
+    const names = [];
+    names.push(stack);
+
+    if (!stack.stacks) {
+      return stack.name;
+    }
+
+    return stack.stacks.map(item => this.getStacks(item)).join(",");
+  };
+
+  getRoutes = (stack: Stack): string => {
+    const names = [];
+    names.push(stack);
+
+    if (!stack.stacks) {
+      return stack.routes!.map(item => item.key).join(",");
+    }
+
+    return stack.stacks.map(item => this.getRoutes(item)).join(",");
+  };
+
+  goBack = () => {
+    if (this.state.currentRoute.previousRoute) {
+      const prev = this.state.currentRoute.previousRoute;
+      this.setState({ currentRoute: prev, currentStack: prev.parentStack });
+      return true;
+    }
+
+    return false;
+  };
+
+  goHome = () => {
+    if (this.state.currentRoute != this.state.currentStack.initial) {
+      this.setState({ currentRoute: this.state.currentStack.initial! });
+      return true;
+    }
+    return false;
+  };
+
+  navigate = (route: string, props: any) => {
+    const routes = this.state.currentStack.routes!.filter(
+      item => item.key === route
+    );
+
+    if (routes.length === 1) {
+      const currentRoute = routes[0];
+      currentRoute.previousRoute = this.state.currentRoute;
+      this.setState({ currentRoute, currentProps: props });
+      return true;
+    }
+    return false;
+  };
+
+  navigateFromRoot = (stacks: string[], key: string, props: any) => {
+    let currentStack = this.navigation.rootStack;
+
+    for (let i in stacks) {
+      if (currentStack.name === stacks[Number(i)]) {
+        if (currentStack.routes) {
+          const route = currentStack.routes!.filter(item => item.key === key);
+          if (route.length === 1) {
+            const currentRoute = route[0];
+            currentRoute.previousRoute = this.state.currentRoute;
+
+            this.setState({
+              currentRoute,
+              currentStack,
+              currentProps: props
+            });
+            return true;
+          }
+        }
+      }
+      const st = currentStack.stacks!.filter(
+        s => s.name === stacks[Number(i) + 1]
+      );
+      if (st.length === 1) {
+        currentStack = st[0];
+        continue;
+      }
+      return false;
+    }
+
+    return false;
+  };
+
+  constructor(props: Props) {
+    super(props);
+
+    const rootStack = this.getStack(props.stack);
+    let initialStack = rootStack;
+    if (!props.stack.routes || props.stack.routes.length === 0) {
+      if (!props.stack.initialStack) {
+        throw new Error(
+          "Your current stack, has not routes installed. Please provide 'initialStack' parameter to get the initial routes from"
+        );
+      }
+      initialStack = rootStack.stacks!.filter(
+        item => item.name === props.stack.initialStack
+      )[0];
+    }
+
+    this.navigation = {
+      rootStack: rootStack,
+      initialStack: initialStack,
+      initialRoute: initialStack.initial!,
+      getStacks: () => this.getStacks(rootStack),
+      getRoutes: () => this.getRoutes(rootStack),
+      getCurrentRoute: () => this.state.currentRoute.key,
+      getCurrentStack: () => this.state.currentStack.name,
+      goBack: this.goBack,
+      goHome: this.goHome,
+      navigate: this.navigate,
+      navigateFromRoot: this.navigateFromRoot
+    };
+
+    BackHandler.addEventListener("hardwareBackPress", () => {
+      if (
+        !(
+          this.state.currentStack === this.navigation.rootStack &&
+          this.state.currentRoute === this.navigation.initialRoute
+        )
+      ) {
+        this.goBack();
+        return true;
+      }
+    });
+
+    this.state = {
+      currentStack: initialStack,
+      currentRoute: this.navigation.initialRoute,
+      currentProps: null
+    };
+  }
+
+  render = () => {
+    return (
+      <View style={this.props.style}>
+        <this.state.currentRoute.route
+          {...this.state.currentProps}
+          navigation={this.navigation}
+        />
+      </View>
+    );
+  };
+}
+
+interface NlndNavigationProps {
+  navigation: Navigation;
+}
+
+export { NlndNavigation, NlndNavigationProps };
